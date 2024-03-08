c6f12ae1fd5f5a1beaf586828d17951519ca2a3c
munjji <ehowl1117@naver.com>
2024-03-08 17:10:39 +0900

    이지희 2021113406

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 name = input('이름: ')
 studentId = input('학번: ')
+major = input('학과: ')
 
-print("학생의 이름 : " + name + " 학번 : " + studentId)
+print("학생의 이름 : " + name + " 학번 : " + studentId + " 전공 : " + major)
